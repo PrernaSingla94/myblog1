e3f9d179da8478773790a5f46b7f7fc8e3ee83e6
PrernaGoyal <prernasingla94@gmail.com>
2024-01-04 13:26:39 +0530

    127:Create Save Post Feature Entity

---
+++ src/main/java/com/myblog1/myblog1/entity/Post.java
@@ -0,0 +1,24 @@
+package com.myblog1.myblog1.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="posts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+
+}
